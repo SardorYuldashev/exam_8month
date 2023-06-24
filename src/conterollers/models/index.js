26b812a5a7ba3b5f1f51809a71227c2aeea01acf
Sardor <henkook-baron@mail.ru>
2023-06-24 21:49:32 +0500

    create getModels controller

--- src/conterollers/models/index.js
+++ src/conterollers/models/index.js
@@ -11,7 +11,7 @@ const addModel = async (req, res, next) => {
   try {
     const { name, brand_id } = req.body;
 
-    const result = await db('models').insert({name, brand_id}).returning('*');
+    const result = await db('models').insert({ name, brand_id }).returning('*');
 
     res.status(201).json({
       model: result[0]
@@ -21,6 +21,57 @@ const addModel = async (req, res, next) => {
   };
 };
 
+/**
+ * Modellar listini olish
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @param {express.NextFunction} next 
+ */
+const getModels = async (req, res, next) => {
+  try {
+    const { q, offset = 0, limit = 10, sort_by = 'id', sort_order = 'desc' } = req.query;
+
+    const dbQuery = db('models')
+      .leftJoin('brands', 'brands.id', 'models.brand_id')
+      .select(
+        'models.id',
+        'models.name',
+        db.raw(`
+          CASE WHEN models.brand_id IS NULL THEN NULL
+          ELSE json_build_object(
+            'id', brands.id,
+            'name', brands.name
+          ) END as brand
+        `)      
+      )
+      .groupBy('models.id', 'brands.id');
+
+    if (q) {
+      dbQuery.andWhereILike('models.name', `%${q}%`);
+    };
+
+    const total = await dbQuery.clone().count().groupBy('models.id');
+
+    dbQuery.orderBy(sort_by, sort_order);
+
+    dbQuery.limit(limit).offset(offset);
+
+    const models = await dbQuery;
+
+    res.status(200).json({
+      models,
+      pageInfo: {
+        total: total.length,
+        offset,
+        limit,
+      }
+    });
+  } catch (error) {
+    next(error);
+  };
+};
+
 module.exports = {
-  addModel
+  addModel,
+  getModels
 };
--- src/routes/models.js
+++ src/routes/models.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const genValidator = require('../shared/validator');
 const { isLoggedIn } = require('../shared/auth');
 const schemas = require('../conterollers/models/schemas');
-const { addModel } = require('../conterollers/models');
+const { addModel, getModels } = require('../conterollers/models');
 
 const mAddModel = [isLoggedIn, genValidator(schemas.addModelSchema)];
 
 router.post('/models', mAddModel, addModel);
+router.get('/models', getModels);
 
 module.exports = router;
