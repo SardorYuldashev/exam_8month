a1ed00653e7f04d67ddca73ee6136bde0bdd87fb
Sardor <henkook-baron@mail.ru>
2023-06-24 18:10:12 +0500

    create addCategory controller

--- src/conterollers/categories/index.js
+++ src/conterollers/categories/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const db = require('../../db');
+const { BadReqqustError } = require('../../shared/errors');
 
 /**
  * Kategoriya yaratish
@@ -10,7 +12,11 @@ const addCategory = async (req, res, next) => {
   try {
     const { name } = req.body;
 
-    res.sendStatus(200);
+    const result = await db('categories').insert({name}).returning('*');
+    
+    res.status(200).json({
+      category: result[0]
+    });
   } catch (error) {
     next(error);
   };
@@ -23,10 +29,48 @@ const addCategory = async (req, res, next) => {
  * @param {express.NextFunction} next 
  */
 const getCategories = async (req, res, next) => {
+  try {
+    const { q, offset = 0, limit = 10, sort_by = 'id', sort_order = 'desc' } = req.query;
 
+    const dbQuery = db('categories').select('*');
+
+    if (q) {
+      dbQuery.andWhereILike('name', `%${q}%`);
+    };
+
+    const total = await dbQuery.clone().count().groupBy('id');
+
+    dbQuery.orderBy(sort_by, sort_order);
+
+    dbQuery.limit(limit).offset(offset);
+
+    const categories = await dbQuery;
+
+    res.status(200).json({
+      categories,
+      pageInfo: {
+        total: total.length,
+        offset,
+        limit,
+      }
+    });
+  } catch (error) {
+    next(error);
+  };  
 };
+
+/**
+ * Bitta kategoriyani olish
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @param {express.NextFunction} next 
+ */
+const showCategory = async (req, res, next) => {
+
+}
 
 module.exports = {
   addCategory,
-  getCategories
+  getCategories,
+  showCategory
 };
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const { isLoggedIn } = require('../shared/auth');
-const { addCategory } = require('../conterollers/categories');
+const { addCategory, getCategories } = require('../conterollers/categories');
 const schemas = require('../conterollers/categories/schemas');
 const genValidator = require('../shared/validator');
 
 const mPostCategory = [isLoggedIn, genValidator(schemas.addCategorySchema)];
 
 router.post('/categories', mPostCategory, addCategory);
+router.get('/categories', getCategories);
 
 module.exports = router;
