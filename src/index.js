da951051c48b15b0f5e0f3b4a0b14701ee4c6dc0
Sardor <henkook-baron@mail.ru>
2023-06-24 12:57:03 +0500

    create app server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log(`http://localhost:3000`);
+});
