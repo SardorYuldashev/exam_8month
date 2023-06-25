d5a5f84349d2c25735144b31bacf32b6f04000b0
Sardor <henkook-baron@mail.ru>
2023-06-25 16:29:55 +0500

    edit seeds

--- seeds/03brands.js
+++ seeds/03brands.js
@@ -5,9 +5,9 @@
 exports.seed = async function(knex) {
   await knex('brands').del()
   await knex('brands').insert([
+    {name: 'Apple'},
     {name: 'HP'},
     {name: 'Dell'},
-    {name: 'Apple'},
     {name: 'Asus'},
     {name: 'Lenovo'},
     {name: 'Acer'}
