24467cb1ab512560f7bfce424772bbb834b2f190
Sardor <henkook-baron@mail.ru>
2023-06-25 15:28:13 +0500

    create getNoutbooks router

--- src/conterollers/noutbooks/index.js
+++ src/conterollers/noutbooks/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../../db');
 
 /**
- * 
+ * Noutbook qo'shish
  * @param {express.Request} req 
  * @param {express.Response} res 
  * @param {express.NextFunction} next 
@@ -11,7 +11,7 @@ const addNoutbook = async (req, res, next) => {
   try {
     const { ...values } = req.body;
 
-    if(req.file) {
+    if (req.file) {
       values.image = req.file.filename;
     };
 
@@ -25,6 +25,68 @@ const addNoutbook = async (req, res, next) => {
   };
 };
 
+/**
+ * Noutbooklar listini olish
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @param {express.NextFunction} next 
+ */
+const getNoutbooks = async (req, res, next) => {
+  try {
+    const { q, offset = 0, limit = 10, sort_by = 'id', sort_order = 'desc' } = req.query;
+
+    const dbQuery = db('noutbooks')
+      .leftJoin('brands', 'brands.id', 'noutbooks.brand_id')
+      .leftJoin('models', 'models.id', 'noutbooks.model_id')
+      .select(
+        'noutbooks.id',
+        'noutbooks.name',
+        'noutbooks.price',
+        'noutbooks.description',
+        'noutbooks.image',
+        db.raw(`
+          CASE WHEN noutbooks.brand_id IS NULL THEN NULL
+          ELSE json_build_object(
+            'id', brands.id,
+            'name', brands.name
+          ) END as brand
+        `),
+        db.raw(`
+          CASE WHEN noutbooks.model_id IS NULL THEN NULL
+          ELSE json_build_object(
+            'id', models.id,
+            'name', models.name
+          ) END as model
+        `)
+      )
+      .groupBy('noutbooks.id', 'brands.id', 'models.id');
+
+    if (q) {
+      dbQuery.andWhereILike('noutbooks.name', `%${q}%`);
+    };
+
+    const total = await dbQuery.clone().count().groupBy('noutbooks.id');
+
+    dbQuery.orderBy(sort_by, sort_order);
+
+    dbQuery.limit(limit).offset(offset);
+
+    const noutbooks = await dbQuery;
+
+    res.status(200).json({
+      noutbooks,
+      pageInfo: {
+        total: total.length,
+        offset,
+        limit,
+      }
+    });
+  } catch (error) {
+    next(error);
+  };
+};
+
 module.exports = {
-  addNoutbook
+  addNoutbook,
+  getNoutbooks
 };
--- src/routes/noutbooks.js
+++ src/routes/noutbooks.js
@@ -3,10 +3,11 @@ const upload = require('../shared/fileUpload');
 const uploadValidator = require('../shared/validator/uploadValidator');
 const schemas = require('../conterollers/noutbooks/schemas');
 const { isLoggedIn } = require('../shared/auth');
-const { addNoutbook } = require('../conterollers/noutbooks');
+const { addNoutbook, getNoutbooks } = require('../conterollers/noutbooks');
 
 const mAddNoutbook = [isLoggedIn, upload.single('photos'), uploadValidator(schemas.addNoutbookSchema)];
 
 router.post('/noutbooks', mAddNoutbook, addNoutbook);
+router.get('/noutbooks', getNoutbooks);
 
 module.exports = router;
