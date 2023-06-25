d5a64c5eb5c17e43f0deaee6c21ae5b58a40e918
Sardor <henkook-baron@mail.ru>
2023-06-25 23:14:18 +0500

    add path notFound middlware

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,16 @@ const categoriesRoutes = require('./categories');
 const brandsRoutes = require('./brands');
 const modelsRoutes = require('./models');
 const noutbooksRoutes = require('./noutbooks');
+const { NotFoundError } = require('../shared/errors');
 
 router.use(usersRoutes);
 router.use(categoriesRoutes);
 router.use(brandsRoutes);
 router.use(modelsRoutes);
 router.use(noutbooksRoutes);
+
+router.use((req, res, next) => {
+  throw new NotFoundError(`Mavjud bo'lmagan yo'l`);
+});
 
 module.exports = router;
