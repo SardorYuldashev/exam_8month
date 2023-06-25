51e5722b3c5a3440f72e57d23c1f2bb0d1138999
Sardor <henkook-baron@mail.ru>
2023-06-25 19:24:53 +0500

    edit getCategory and showCategory. added noutbook info to response

--- src/conterollers/categories/index.js
+++ src/conterollers/categories/index.js
@@ -33,13 +33,30 @@ const getCategories = async (req, res, next) => {
   try {
     const { q, offset = 0, limit = 10, sort_by = 'id', sort_order = 'desc' } = req.query;
 
-    const dbQuery = db('categories').select('*');
+    const dbQuery = db('categories')
+      .leftJoin('noutbooks_categories', 'noutbooks_categories.category_id', 'categories.id')
+      .leftJoin('noutbooks', 'noutbooks_categories.noutbook_id', 'noutbooks.id')
+      .select(
+        'categories.id',
+        'categories.name',
+        db.raw(`
+        COALESCE(
+          json_agg(
+            json_build_object(
+            'id', noutbooks.id,
+            'name', noutbooks.name,
+            'price', noutbooks.price
+          ) 
+        ) filter (where noutbooks_categories.noutbook_id IS NOT NULL), '[]') as noutbooks
+        `)
+      )
+      .groupBy('categories.id');
 
     if (q) {
-      dbQuery.andWhereILike('name', `%${q}%`);
+      dbQuery.andWhereILike('categories.name', `%${q}%`);
     };
 
-    const total = await dbQuery.clone().count().groupBy('id');
+    const total = await dbQuery.clone().count().groupBy('categories.id');
 
     dbQuery.orderBy(sort_by, sort_order);
 
@@ -70,7 +87,26 @@ const showCategory = async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    const category = await db('categories').select('*').where({ id }).first();
+    const category = await db('categories')
+      .leftJoin('noutbooks_categories', 'noutbooks_categories.category_id', 'categories.id')
+      .leftJoin('noutbooks', 'noutbooks_categories.noutbook_id', 'noutbooks.id')
+      .select(
+        'categories.id',
+        'categories.name',
+        db.raw(`
+        COALESCE(
+          json_agg(
+            json_build_object(
+            'id', noutbooks.id,
+            'name', noutbooks.name,
+            'price', noutbooks.price
+          )
+        ) filter (where noutbooks_categories.noutbook_id IS NOT NULL), '[]') as noutbooks
+        `)
+      )
+      .where({ 'categories.id': id })
+      .groupBy('categories.id')
+      .first();
 
     if (!category) {
       throw new NotFoundError(`IDsi ${id} bo'lgan kategoriya topilmadi`);
@@ -125,14 +161,14 @@ const deleteCategory = async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    const existing = await db('categories').where({id}).first();
+    const existing = await db('categories').where({ id }).first();
 
-    if(!existing) {
+    if (!existing) {
       throw new NotFoundError(`IDsi ${id} bo'lgan kategoriya topilmadi`);
     };
 
     const deleted = await db('categories')
-      .where({id})
+      .where({ id })
       .delete()
       .returning('*');
 
