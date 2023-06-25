58ca80497d0a482e1d5ca4d852fd504a0a201e6c
Sardor <henkook-baron@mail.ru>
2023-06-25 17:37:58 +0500

    create noutbooks_categories migration

---
+++ migrations/20230625123323_noutbooks_categories.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('noutbooks_categories', (table) => {
+    table.increments('id').primary();
+    table.integer('noutbook_id').references('id').inTable('noutbooks');
+    table.integer('category_id').references('id').inTable('categories');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('noutbooks_categories');
+};
